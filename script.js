3bc24c26027d40cd076e417c43ceff98673c542e
Elena Cristescu <elenacristescu11@gmail.com>
2023-10-19 19:10:12 +0200

    examples of functions accepting callback functions

--- script.js
+++ script.js
@@ -23,29 +23,58 @@
 // createBooking('LH123',5)
 
 
-const flight = 'LH234';
-const elena = {
-    name:'Elena Cristescu',
-    passport:172927301,
+// const flight = 'LH234';
+// const elena = {
+//     name:'Elena Cristescu',
+//     passport:172927301,
+// }
+
+// const checkIn = function(flightNum,passenger){
+//     flightNum='LH999',
+//     passenger.name='Ms. '+ passenger.name;
+
+//     if(passenger.passport===172927301){
+//         alert('Check In')
+//     }else{
+//         alert('Wrong passport!!')
+//     }
+// }
+
+// checkIn(flight,elena);
+// console.log(flight);
+// console.log(elena);
+
+// const newPassport = function(person){
+//     person.passport = Math.trunc(Math.random()*10000000000000);
+// };
+// newPassport(elena);
+// checkIn(flight,elena);
+
+const oneWord = function(str){
+    return str.replace(/ /g,'').toLowerCase();
 }
 
-const checkIn = function(flightNum,passenger){
-    flightNum='LH999',
-    passenger.name='Ms. '+ passenger.name;
-
-    if(passenger.passport===172927301){
-        alert('Check In')
-    }else{
-        alert('Wrong passport!!')
-    }
+const upperFirstWord = function(str){
+    const [first,...other] = str.split(' ');
+    return [first.toUpperCase(),...other].join(' ');
 }
 
-checkIn(flight,elena);
-console.log(flight);
-console.log(elena);
+const transformer = function(str,fn){
+    console.log(`Original string: ${str}`);
+console.log(`Transformed string: ${fn(str)}`);
 
-const newPassport = function(person){
-    person.passport = Math.trunc(Math.random()*10000000000000);
-};
-newPassport(elena);
-checkIn(flight,elena);
+console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!',upperFirstWord);
+transformer('JavaScript is the best!',oneWord);
+
+
+//JS callbacks
+const high5 = function(){
+    console.log('✋');
+}
+
+document.body.addEventListener('click',high5);
+
+['Elena','Martha','Adam'].forEach(high5);
