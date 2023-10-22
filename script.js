7939ff34597a6bd8bf912872ef5e73bcf640fe73
Elena Cristescu <elenacristescu11@gmail.com>
2023-10-22 18:16:15 +0200

    example of Immediatialey Invoked Function Expresion

--- script.js
+++ script.js
@@ -96,118 +96,140 @@
 
 //The call and apply Method
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  //book:function(){}
-  book(flightNum, name) {
-    console.log(
-      `${name}  booked a seat on ${this.airline} fligh ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   //book:function(){}
+//   book(flightNum, name) {
+//     console.log(
+//       `${name}  booked a seat on ${this.airline} fligh ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+
+// lufthansa.book(234, 'Elena Cristescu');
+// lufthansa.book(673, 'John Smith');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// const book = lufthansa.book;
+// //Does NOT Work
+// //book(23, 'Sarah Williams');
+
+// //CALL Method
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
+
+// book.call(lufthansa, 247, 'Mary Copper');
+// console.log(lufthansa);
+
+// const swiss = {
+//   airline: 'Swiss Air Line',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+// book.call(swiss, 753, 'Mary Cooper');
+// console.log(swiss);
+
+// const flightData = [575, 'George Cooper'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+
+// //bind method
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+
+// bookEW(23, 'Steven Williams');
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Ellen Kris');
+// bookEW23('Martha Cooper');
+
+// //With Event Listener
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// //lufthansa.buyPlane();
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// //Partial Application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+
+// console.log(addVAT(100));
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+// const addVAT2 = addTaxRate(0.23);
+
+// console.log(addVAT2(100));
+
+// //Challenge 1
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
+// };
+
+// poll.registerNewAnswer = function () {
+//   let answer = +prompt(
+//     'What is your favourite programming language?\n 0: JavaScript \n 1: Python \n 2: Rust \n 3: C++\n(Write option number)'
+//   );
+//   if (answer && typeof answer === 'number' && answer > -1 && answer < 4) {
+//     // console.log(answer, 'call');
+//     poll.answers[answer]++;
+//     console.log(poll);
+//   } else {
+//     answer = prompt(
+//       'Please select one of the following options: \n What is your favourite programming language?\n 0: JavaScript \n 1: Python \n 2: Rust \n 3: C++\n(Write option number)'
+//     );
+//   }
+// };
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// // poll.registerNewAnswer();
+// // console.log(poll);
+const runOnce = function () {
+  console.log('This will never run again');
 };
+runOnce();
 
-lufthansa.book(234, 'Elena Cristescu');
-lufthansa.book(673, 'John Smith');
-console.log(lufthansa);
+//IIFE Immediatialey Invoked Function Expresion
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// console.log(isPrivate);
 
-const book = lufthansa.book;
-//Does NOT Work
-//book(23, 'Sarah Williams');
+(() => console.log('This ALSO will never run again'))();
 
-//CALL Method
-book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
-
-book.call(lufthansa, 247, 'Mary Copper');
-console.log(lufthansa);
-
-const swiss = {
-  airline: 'Swiss Air Line',
-  iataCode: 'LX',
-  bookings: [],
-};
-book.call(swiss, 753, 'Mary Cooper');
-console.log(swiss);
-
-const flightData = [575, 'George Cooper'];
-book.apply(swiss, flightData);
-console.log(swiss);
-
-//bind method
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-
-bookEW(23, 'Steven Williams');
-
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('Ellen Kris');
-bookEW23('Martha Cooper');
-
-//With Event Listener
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
-};
-//lufthansa.buyPlane();
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
-
-//Partial Application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(null, 0.23);
-
-console.log(addVAT(100));
-
-const addTaxRate = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
-};
-const addVAT2 = addTaxRate(0.23);
-
-console.log(addVAT2(100));
-
-//Challenge 1
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
-  // This generates [0, 0, 0, 0]. More in the next section!
-  answers: new Array(4).fill(0),
-};
-
-poll.registerNewAnswer = function () {
-  let answer = +prompt(
-    'What is your favourite programming language?\n 0: JavaScript \n 1: Python \n 2: Rust \n 3: C++\n(Write option number)'
-  );
-  if (answer && typeof answer === 'number' && answer > -1 && answer < 4) {
-    // console.log(answer, 'call');
-    poll.answers[answer]++;
-    console.log(poll);
-  } else {
-    answer = prompt(
-      'Please select one of the following options: \n What is your favourite programming language?\n 0: JavaScript \n 1: Python \n 2: Rust \n 3: C++\n(Write option number)'
-    );
-  }
-};
-
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
-
-// poll.registerNewAnswer();
-// console.log(poll);
+{
+  const isPrivate = 23;
+  //var variables ignore blocks
+  var notPrivate = 12;
+}
+//console.log(isPrivate);
+console.log(notPrivate);
